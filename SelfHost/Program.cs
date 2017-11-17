b2cdc501f5a992133c21f66f525a041305f28686
pankaj dhami <p.dhami@agilecockpit.com>
2017-11-17 13:42:51 +0530

    Add project files.

---
+++ SelfHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
+
+namespace SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string baseAddress = "http://localhost:80/";
+
+            //// Start OWIN host 
+            //using (WebApp.Start<Startup>(url: baseAddress))
+            //{
+            //    // Create HttpCient and make a request to api/values 
+            //    HttpClient client = new HttpClient();
+
+            //    var response = client.GetAsync(baseAddress + "api/values").Result;
+
+            //    Console.WriteLine(response);
+            //    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            //    Console.ReadLine();
+            //}
+        }
+    }
+}
---
+++ SelfHost/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.Services.Description;
+[assembly: OwinStartup(typeof(SelfHost.Startup))]
+
+namespace SelfHost
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
